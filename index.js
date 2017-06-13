ddb7358410c28d60f15141ff573a1377e4fe7f20
yangzhanmei <15809244435@163.com>
2017-06-13 18:07:47 +0800

    let it's parent hidden when click hidden dom

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ $(function () {
     $(".list > li > a").click(function () {
         $(this).addClass("current").siblings().show().parent().siblings().children("a").removeClass("current").next().hide();
     });
+    $("span:hidden").click(function () {
+      $(this).parent().hide();
+    });
 });
