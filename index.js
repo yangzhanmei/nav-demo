5f8f3e53765e185b4783f183a7e4b0bbc7633cc7
yangzhanmei <15809244435@163.com>
2017-06-13 17:22:14 +0800

    add dropDownBox

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 $(function () {
-   $(".list > li > a").click(function () {
-       $(this).addClass("current").parent().siblings().children("a").removeClass("current");
-   })
+    $(".liList").hide();
+    $(".list > li > a").click(function () {
+        $(this).addClass("current").parent().siblings().children("a").removeClass("current");
+    });
 });
